ea9d86cd4c7f360346d34fe6fb5126e52fadf931
Your Name <fosem@gmail.com>
2026-03-13 14:26:53 +0900

    lombokí활용 및 리팩토링

---
+++ src/test06/Bubble.java
@@ -0,0 +1,107 @@
+package test06;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.swing.*;
+
+@Getter
+@Setter
+public class Bubble extends JLabel implements Moveable {
+
+    private int x;
+    private int y;
+
+    private ImageIcon bubbleIcon;
+    private Player player;
+
+    //버블 이동 상태 플래그
+    private static final int HORIZONTAL_DISTANCE = 400; // 수평 이동 거리
+    private static final int BUBBLE_SPEED_MS = 1; // 이동 간격
+    private static final int SCREEN_TOP = 0; // 화면 상단 경계
+
+    private boolean leftMoving = false;
+    private boolean rightMoving = false;
+    private boolean upMoving = false;
+
+
+    public Bubble(Player player) {
+        this.player = player;
+        initData();
+        setInitLayout();
+        bubbleStartTharead();
+    }
+
+    // 물방울 이동 Thread 시작
+    public void bubbleStartTharead() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (player.getPlayerWay() == PlayerWay.LEFT) {
+                    left(); // 왼쪽으로 400px 이동 완료 후 ---> up 호출
+                } else {
+                    right(); // 오른쪽으로 400px 이동 완료 후 ---> up 호출
+                }
+            }
+        }).start();
+    }
+
+    private void initData() {
+        bubbleIcon = new ImageIcon("img/bubble.png");
+    }
+
+    private void setInitLayout() {
+        x = player.getX();
+        y = player.getY();
+        setIcon(bubbleIcon);
+        setSize(50, 50);
+        setLocation(x, y);
+        setVisible(true);
+    }
+
+    @Override
+    public void left() {
+        leftMoving = true;
+        for (int i = 0; i < HORIZONTAL_DISTANCE; i++) {
+            x--;
+            setLocation(x, y);
+            try {
+                Thread.sleep(BUBBLE_SPEED_MS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        leftMoving = false;
+        up(); // 수평 이동 완료 후 -> 상승 시작
+    }
+
+    @Override
+    public void right() {
+        rightMoving = true;
+        for (int i = 0; i < HORIZONTAL_DISTANCE; i++) {
+            x++;
+            setLocation(x, y);
+            try {
+                Thread.sleep(BUBBLE_SPEED_MS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        rightMoving = false;
+        up();
+    }
+
+    @Override
+    public void up() {
+        while (y > SCREEN_TOP) {
+            y --;
+            setLocation(x, y);
+            try {
+                Thread.sleep(BUBBLE_SPEED_MS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        setVisible(false);
+    }
+}
